58f9fa6d5e2d53f97012c709963bf82532fcbfc3
siMarcTahimikLang <andrieverial@gmail.com>
2023-02-15 15:39:00 +0800

    added files

---
+++ js/index.js
@@ -0,0 +1,125 @@
+/*//oncontextmenu
+function first(){
+
+    alert ("you have right clicked a button");
+}
+
+function fist(){
+
+    alert("You have right clicked a link");
+}
+
+
+//onmouseout 
+function second(){
+
+    alert ("you have moved out of the button");
+}
+
+function seco(){
+
+    alert("You have moved out of the link");
+}
+
+
+//onkeydown 
+function third(){
+
+    alert ("you have pressed a key inside text input!");
+}
+
+function thi(){
+
+    alert("you have pressed a key inside textarea!");
+}
+
+
+//onkeyup 
+function four(){
+
+    alert ("you have released a key inside text input");
+}
+
+function fo(){
+
+    alert("you have released a key inside textarea!");
+}
+
+
+//onkeypress 
+function five(){
+
+    alert ("you have pressed a key inside text input");
+}
+
+function fiv(){
+
+    alert("you have pressed a key inside textarea");
+}*/
+
+/*onfocus 
+function five(){
+
+    alert ("you have pressed a key inside text input");
+}
+
+function fiv(){
+
+    alert("you have pressed a key inside textarea");
+}*/
+
+
+/*onblur
+function five(){
+
+    alert ("you have pressed a key inside text input");
+}
+
+function fiv(){
+
+    alert("you have pressed a key inside textarea");
+}
+
+//ONSUBMIT
+function it(){
+
+    alert("Form data will be submmitedn to the server!")
+     
+}
+
+//ONLOAD
+function it(){
+
+    alert("Page is loaded successfully!")
+     
+}*//
+
+
+//onresize
+
+
+
+
+//The if Statement
+    var now=new Date();
+    var dayOfWeek=now.getDay();
+
+        if (dayOfWeek==3){
+            alert("It's Wednesday!");
+
+        }   
+
+//The if...else if...else Statement
+    var now =newDate();
+    var dayOfWeek = now.getDay();// Sunday - Saturday : 0 - 6
+    
+        if(dayOfWeek ==5){
+        alert("Have a nice weekend!");
+        }
+    
+        elseif(dayOfWeek ==0){
+            alert("Have a nice Sunday!");
+        }
+        else{
+            alert("Have a nice day!");
+        }
